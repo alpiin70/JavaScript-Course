6a47a570d91d0d2d94055865a45a834ae0d3cfa9
alpiin70 <alpnpcng4@gmail.com>
2024-11-29 16:10:00 +0700

    Add files via upload

---
+++ 1/main.js
@@ -0,0 +1,50 @@
+// alert('Hello World!');
+// console.log('Hello World');
+// console.error('This Is a error');
+// console.warn('This Is a Warning');
+
+
+// let, const
+
+// const age = 30;
+// console.log(age);
+
+
+// String, Numbers, Boolean, null, undefined
+
+// const name = 'Jhon';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+// console.log(typeof z);
+
+
+// String, Numbers, Boolean, null, undefined
+
+// const name = 'Jhon';
+// const age = 30;
+// // Concatenation
+// console.log('my name is ' + name + " and i am " + age);
+// // Template String
+// const hello = (`my name is ${name} and i am ${age}`);
+// console.log(hello)
+
+
+// const s = 'technology, computers, it, code';
+// console.log(s.length)
+// console.log(s.toUpperCase())
+// console.log(s.toLowerCase())
+// console.log(s.substring(0, 5).toUpperCase())
+// console.log(s.split(''))
+// console.log(s.split(', '))
+
+
+// Arrays - variables that hold multiple values
+
+// const numbers = new Array(1,2,3,4,5);
+// console.log(numbers);
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(fruits);
